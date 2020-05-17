ee83d70039164cb86bb87a28ddedb920cc16e5bb
guzhongren <guzhongren@live.cn>
2020-05-17 16:57:02 +0800

    backstagePass will be expired in 15 days, and quality is 20

---
+++ src/main/java/cn/xpbootcamp/gildedrose/BackstagePass.java
@@ -0,0 +1,32 @@
+package cn.xpbootcamp.gildedrose;
+
+public class BackstagePass {
+    private int sellIn;
+    private int quality;
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public BackstagePass(int sellIn, int quality) {
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public void passOneDay() {
+        this.sellIn --;
+        this.quality ++;
+    }
+}
---
+++ src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
@@ -0,0 +1,16 @@
+package cn.xpbootcamp.gildedrose;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BackstagePassTest {
+
+    @Test
+    public void should_return_sellIn_14_quality_21_when_backstagePass_pass_1_day_given_sellIn_15_quality_20(){
+        BackstagePass backstagePass = new BackstagePass(15,20);
+        backstagePass.passOneDay();
+        assertThat(backstagePass.getSellIn()).isEqualTo(14);
+        assertThat(backstagePass.getQuality()).isEqualTo(21);
+    }
+
+}
