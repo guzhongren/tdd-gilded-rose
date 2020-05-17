2cb81cf782bf798fb6332a23d0798fdecb23b1f6
guzhongren <guzhongren@live.cn>
2020-05-17 17:22:47 +0800

    sulfuras will be expired in -1 day, quality is 1

--- src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
@@ -27,4 +27,11 @@ public class SulfurasTest {
         assertThat(sulfuras.getSellIn()).isEqualTo(-1);
         assertThat(sulfuras.getQuality()).isEqualTo(50);
     }
+    @Test
+    public void should_return_sellIn_negative_1_quality_1_when_sulfuras_pass_1_day_given_sellIn_negative_1_quality_1 () {
+        Sulfuras sulfuras = new Sulfuras(-1,1);
+        sulfuras.passOneDay();
+        assertThat(sulfuras.getSellIn()).isEqualTo(-1);
+        assertThat(sulfuras.getQuality()).isEqualTo(1);
+    }
 }
