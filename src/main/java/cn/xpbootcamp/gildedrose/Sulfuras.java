e39656547ee44038be48d023ca0120105cfc82f1
guzhongren <guzhongren@live.cn>
2020-05-17 17:19:01 +0800

    sulfuras will be expired in 0 day, quality is 45

---
+++ src/main/java/cn/xpbootcamp/gildedrose/Sulfuras.java
@@ -0,0 +1,30 @@
+package cn.xpbootcamp.gildedrose;
+
+public class Sulfuras {
+    private int sellIn;
+    private int quality;
+
+    public Sulfuras(int sellIn, int quality) {
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+    public void passOneDay() {
+
+    }
+}
